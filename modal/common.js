c54a2e296a5f437be626267ecc162e0749aeb59f
yeony.cho <yeony.cho@kakaocorp.com>
2019-05-03 18:03:08 +0900

    modal popup : dimmed 클릭시, 모달 닫기 추가

--- modal/common.js
+++ modal/common.js
@@ -4,14 +4,21 @@
     var btnOpen = document.querySelector(".btn_open");
     var btnClose = document.querySelector(".btn_close");
     var modal = document.querySelector(".sample_layer");
+    var dimModal = document.querySelector(".dimmed_layer");
 
+    //modal open
     function modalOpen(){
         modal.classList.add("on");
+        dimModal.classList.add("on");
     }
+
+    //modal close
     function modalClose(){
         modal.classList.remove("on");
+        dimModal.classList.remove("on");
     }
 
     btnOpen.addEventListener("click", modalOpen);
     btnClose.addEventListener("click", modalClose);
+    dimModal.addEventListener("click", modalClose);
 })();
