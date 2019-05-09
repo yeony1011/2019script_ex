4f48e252cc5bbf96948783e8d2502a5a70991a4d
yeony.cho <yeony.cho@kakaocorp.com>
2019-05-09 16:29:14 +0900

    modal popup 효과 추가

---
+++ modal/README.md
@@ -0,0 +1,9 @@
+# modal 기능
+
+## 자바스크립트
+- [결과화면 보기](https://yeony1011.github.io/2019script_ex/190325/190325_v2.html)
+- [해당 소스 보기](https://github.com/yeony1011/2019script_ex/blob/master/modal/common.js)
+
+## 기능
+- [x] -, + 버튼 제어로 input 숫자 값 증가/감소
+- [x] 0이상일때 증가 O, 0이하일때 감소 X
--- modal/common.js
+++ modal/common.js
@@ -3,19 +3,17 @@
 (function(){
     var btnOpen = document.querySelector(".btn_open");
     var btnClose = document.querySelector(".btn_close");
-    var modal = document.querySelector(".sample_layer");
+    var bodyCont = document.querySelector("body");
     var dimModal = document.querySelector(".dimmed_layer");
 
     //modal open
     function modalOpen(){
-        modal.classList.add("on");
-        dimModal.classList.add("on");
+        bodyCont.classList.add("modal_on");
     }
 
     //modal close
     function modalClose(){
-        modal.classList.remove("on");
-        dimModal.classList.remove("on");
+        bodyCont.classList.remove("modal_on");
     }
 
     btnOpen.addEventListener("click", modalOpen);
