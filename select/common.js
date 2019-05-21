44616b74507dd64338956cf66ca6ef064c968607
yeony.cho <yeony.cho@kakaocorp.com>
2019-05-21 16:10:49 +0900

    디자인된 셀렉트박스 기능 추가

--- select/common.js
+++ select/common.js
@@ -2,13 +2,22 @@
 
 (function(){
     var regionSelect = document.querySelector(".opt_comm");
+    var selectedRegion = document.querySelector(".link_selected");
+    var nowRegion = document.querySelector(".link_option");
 
-    document.querySelector(".link_selected").addEventListener("click", function(){
-        console.log("open");
+    //셀렉트박스 열기
+    selectedRegion.addEventListener("click", function(){
         regionSelect.classList.add("opt_open");
     });
-    document.querySelector(".link_option").addEventListener("click", function(){
-        console.log("close");
-        //regionSelect.classList.remove("opt_open");
+
+    //셀렉트박스 닫기
+    nowRegion.addEventListener("click", function(){
+        var seleVal = this.innerHTML;
+        document.querySelector(".link_selected").innerHTML = seleVal;
+
+        this.parentNode.classList.add("on");
+        regionSelect.classList.remove("opt_open");
+
+        alert("선택된 지역 : "+seleVal);
     });
 })();
